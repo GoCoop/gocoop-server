39f1fec42c12eae73f2e45a474890ad66b18fd03
Carlos Lotus <contato@carloslotuspro.com>
2025-01-28 21:32:41 -0300

    ADD database connection

---
+++ config/database.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func ConnectToDatabase() *pgxpool.Pool {
+
+	var (
+		dbHost     = os.Getenv("DB_HOST")
+		dbPort     = os.Getenv("DB_PORT")
+		dbUser     = os.Getenv("DB_USER")
+		dbPassword = os.Getenv("DB_PASSWORD")
+		dbName     = os.Getenv("DB_NAME")
+	)
+
+	encodedPw := url.QueryEscape(dbPassword)
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, encodedPw, dbHost, dbPort, dbName)
+	fmt.Println(connStr)
+
+	dbpool, err := pgxpool.New(context.Background(), connStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	log.Println("> Connection successful!")
+
+	return dbpool
+}
--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"gocoop-server/config"
 	"gocoop-server/handlers"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file.")
+	}
+
+	log.Println("> Attempting to connect to postgres database...")
+	dbPool := config.ConnectToDatabase()
+	defer dbPool.Close()
+
+	var greeting string
+	err = dbPool.QueryRow(context.Background(), "SELECT 'Hello, World!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /categories", handlers.GetCategories)
 
-	fmt.Println("Server started! Running on port 8080.")
+	log.Println("> Server started! Running on port 8080.")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
